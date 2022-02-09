a341fdaf8abec3965d02d90a7918c6ca571db97c
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-09 11:00:02 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/* Displays contries on 'index.html' */
+function showCountry(country)
+{
+    var countriesList = document.getElementById("country-list");
+    countriesList.innerHTML += "<li>" + country + "</li>";
+}
+
+/* Ajax request to load countries into 'index.html' Table */
+function getCountries()
+{
+    console.log("Loading Countries...");
+
+    var request = new XMLHttpRequest;
+
+    // Preparing request
+    request.open("GET", "https://restcountries.com/v3.1/all", true);
+
+    // Setting Callback
+    request.onreadystatechange = function()
+    {
+        // When request is done, update page
+        if(this.readyState == 4)
+        {
+            console.log("Callback done..." + this.responseText);
+            console.log("Status: " + this.status);
+
+            // if request successful update page
+            if(this.status == 200)
+            {
+                // Parse response
+                var countriesList = JSON.parse(this.responseText);
+
+                // Update the page and display data
+                for(var i in countriesList)
+                {
+                    showCountry(countriesList[i]["name.common"]);
+                }
+            }
+        }
+    }
+
+    // Making the request
+    request.send();
+}
