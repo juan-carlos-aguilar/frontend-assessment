b77ab76e873684ccc03be06004689c46440bad44
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-11 16:42:18 -0600

    lesson-5

--- js/app.js
+++ js/app.js
@@ -1,18 +1,9 @@
-//const wrap = document.querySelector('#wrapper');
-//console.log(wrap);
+var books = document.querySelectorAll('#book-list li .name');
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-//console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-//console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-//console.log(books);
-
-Array.from(books).forEach(function(book)
-{
-    console.log(book);
+Array.from(books).forEach(function(book){
+    book.textContent += ' (book title)';
 });
 
-
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>Books and more books...</h2>'
+bookList.innerHTML += '<p>This is how you add HTML</p>';
