907aead6e96e490536e0563b6a32916a46c6e0f5
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-11 17:45:41 -0600

    lesson-11

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const list = document.querySelector('#book-list ul');
 
+// delete book function
 list.addEventListener('click', function(e){
     if(e.target.className == 'delete')
     {
         const li = e.target.parentElement;
         list.removeChild(li);
     }
+});
+
+// add book function
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type = "text"]').value;
+
+    console.log(value);
 })
