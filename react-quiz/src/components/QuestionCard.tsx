709bcbdbd896f25ab7e09c84c2eabf72e1e2ed64
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-14 17:25:37 -0600

    Finished styling, app working great.

--- react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/src/components/QuestionCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Types import
 import { AnswerObject } from '../App';
 // Styles import
-
+import { Wrapper, ButtonWrapper } from './QuestionCard.style'
 // Specifying props that will be used in the program for functionality.
 type Props = {
     question: string;
@@ -21,21 +21,25 @@ const QuestionCard: React.FC<Props> = ({
     questionNr, 
     totalQuestions}) => (
 
-    <div>
+    <Wrapper>
         <p className="number">
             Question: {questionNr} / {totalQuestions}
         </p>
         <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
             {answers.map ((answer) => (
-                <div key={answer}>
+                <ButtonWrapper 
+                    key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}
+                >
                     <button disabled={!!userAnswer} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer}} />
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div>
+    </Wrapper>
 );
 
 export default QuestionCard;
