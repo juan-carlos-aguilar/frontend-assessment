3b66b547a0fa791e86eae2e41032f2cfc543dd4a
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-14 16:56:29 -0600

    Score will now be displayed. Game working great. Moving on to styling

--- react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/src/components/QuestionCard.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+// Types import
+import { AnswerObject } from '../App';
+// Styles import
 
 // Specifying props that will be used in the program for functionality.
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -26,7 +29,7 @@ const QuestionCard: React.FC<Props> = ({
         <div>
             {answers.map ((answer) => (
                 <div key={answer}>
-                    <button disabled={userAnswer} value={answer} onClick={callback}>
+                    <button disabled={!!userAnswer} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer}} />
                     </button>
                 </div>
