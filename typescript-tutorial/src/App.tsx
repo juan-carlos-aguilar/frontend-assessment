2d88a294d98cbe98a870a2cbf730005f68a73237
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-16 10:20:30 -0600

    Setting up app and adding CSS

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printName: (name: string) => never;
-
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-
-// let person: Person = {
-//   name: "Piyush",
-// };
-
-// let lostOfPeople: Person[];
-
-// let personName: unknown;
-
-interface Person extends X {
-  name: string;
-  age?: number;
-}
-
-type X = {
-  a: string;
-  b: number;
-}
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className="heading">
+        Taskify
+      </span>
     </div>
   );
 }
