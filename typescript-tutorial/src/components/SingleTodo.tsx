c8a5d949a2738bb8c33047106e1346703eb25a7e
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-16 12:17:51 -0600

    Added 'isDone' functionality

--- typescript-tutorial/src/components/SingleTodo.tsx
+++ typescript-tutorial/src/components/SingleTodo.tsx
@@ -3,22 +3,38 @@ import { Todo } from "../model";
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
 import './styles.css';
+import TodoList from './TodoList';
 
 type Props = {
     todo: Todo;
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
+
 const SingleTodo = ({todo, todos, setTodos }: Props) => {
+    const handleDone = (id: number) => {
+        setTodos(
+            todos.map((todo) => 
+                todo.id === id ? { ...todo, isDone : !todo.isDone } : todo
+            )
+        );
+    };
+
   return (
     <form action="" className="todos_single">
-        <span className="todos_single_text">{todo.todo}</span>
+        {
+            todo.isDone ? (
+                <s className="todos_single_text">{todo.todo}</s>        
+            ) : (
+                <span className="todos_single_text">{todo.todo}</span>
+            )
+        }   
         
         {/* React Icons */}
         <div>
             <span className="icon"><AiFillEdit /></span>
             <span className="icon"><AiFillDelete /></span>
-            <span className="icon"><MdDone /></span>
+            <span className="icon" onClick={() => handleDone(todo.id)}><MdDone /></span>
         </div>
     </form>
   )
