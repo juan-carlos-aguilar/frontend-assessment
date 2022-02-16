739ac6a445b78aa0342180dc577407db5da80425
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-16 10:14:25 -0600

    Working with different types of variables

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,4 +1,36 @@
+import React from 'react';
 import './App.css';
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let printName: (name: string) => never;
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: "Piyush",
+// };
+
+// let lostOfPeople: Person[];
+
+// let personName: unknown;
+
+interface Person extends X {
+  name: string;
+  age?: number;
+}
+
+type X = {
+  a: string;
+  b: number;
+}
 
 function App() {
   return (
