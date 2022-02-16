507316832e39f8d23a92f68a8dee7e4f69a38caa
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-16 10:54:18 -0600

    Input field not takes input and displays it on the console

--- typescript-tutorial/src/components/InputField.tsx
+++ typescript-tutorial/src/components/InputField.tsx
@@ -4,11 +4,18 @@ import './styles.css';
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form action="" className="input">
-        <input type="input" placeholder="Enter a Task" className="input_box" />
+    <form onSubmit={handleAdd} className="input">
+        <input type="input" 
+            value={todo}
+            onChange={
+                (e) => setTodo(e.target.value)
+            }
+            placeholder="Enter a Task" 
+            className="input_box" />
         <button className="input_submit" type="submit">
             Go
         </button>
